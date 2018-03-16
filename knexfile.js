284fda80a647ca14d75bdbc21f378603dc8acebb
Christopher <cjszk@hotmail.com>
2018-03-16 11:10:32 -0700

    test

--- knexfile.js
+++ knexfile.js
@@ -3,13 +3,13 @@
 module.exports = {
   development: {
     client: 'pg',
-    connection: process.env.DATABASE_URL || 'postgres://dev:dev123@localhost/noteful-app',
+    connection: process.env.DATABASE_URL || 'postgres://chris:cjdrnch65@localhost/noteful-app',
     debug: true, // http://knexjs.org/#Installation-debug
     pool: {min : 1 , max : 2}
   },
   test: {
     client: 'pg',
-    connection: process.env.TEST_DATABASE_URL || 'postgres://dev:dev123@localhost/notefulTest',
+    connection: process.env.TEST_DATABASE_URL || 'postgres://chris:cjdrnch65@localhost/noteful-test',
     pool: {min : 1 , max : 2}
   },
   production: {
